05f7312da9838703ff805216a671da419a514efb
vasyaqwe <elekton.fafik@gmail.com>
2025-10-01 13:43:13 +0300

    add smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk, Source_Serif_4 } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import LenisScrollProvider from "@/components/smooth-scroll";
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -127,7 +128,7 @@ export default function RootLayout({
           enableSystem={false}
           disableTransitionOnChange
         >
-          {children}
+           <LenisScrollProvider>{children}</LenisScrollProvider>
         </ThemeProvider>
       </body>
     </html>
